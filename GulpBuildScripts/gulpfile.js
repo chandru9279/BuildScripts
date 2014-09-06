f97eedc8c67686bc553f896c60e312650caffb2e
T Chandirasekar <chandru9279@gmail.com>
2014-09-07 01:13:06 +0530

    task to minify, bundle, and hash css

--- GulpBuildScripts/gulpfile.js
+++ GulpBuildScripts/gulpfile.js
@@ -1,5 +1,15 @@
 ﻿var gulp = require('gulp');
+var minifyCSS = require('gulp-minify-css');
+var concat = require('gulp-concat');
+var rev = require('gulp-rev');
+var env = require('./Environments/dev.json');
 
-gulp.task('default', function () {
-    console.log('Hello world');
+gulp.task('default', ['minify-css']);
+
+gulp.task('minify-css', function () {
+    return gulp.src(env.solutionPath + '/Content/*.css')
+    .pipe(minifyCSS({keepSpecialComments :0}))
+    .pipe(concat("css-combined.min.css"))
+    .pipe(rev())
+    .pipe(gulp.dest(env.solutionPath + 'Out/'));
 });
