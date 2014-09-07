00b52851a2669040d2cd97f690195463270b42df
T Chandirasekar <chandru9279@gmail.com>
2014-09-07 18:51:52 +0530

    Adding msbuild task

---
+++ GulpBuildScripts/Conventions.js
@@ -0,0 +1,19 @@
+﻿var env = require('./Environments/dev.json');
+
+module.exports = {
+    solutionPath: function () {
+        return env.repoPath + "Source/";
+    },    
+    buildPath: function () {
+        return env.repoPath + "Build/";
+    },
+    webPath: function () {
+        return this.solutionPath() + "Web/";
+    },
+    solutionFile: function () {
+        return this.solutionPath() + "Jetstar-DotRez.sln";
+    },
+    webProjectFile: function () {
+        return this.webPath() + "Web.csproj";
+    }
+}
--- GulpBuildScripts/gulpfile.js
+++ GulpBuildScripts/gulpfile.js
@@ -2,14 +2,35 @@
 var minifyCSS = require('gulp-minify-css');
 var concat = require('gulp-concat');
 var rev = require('gulp-rev');
-var env = require('./Environments/dev.json');
+var msbuild = require('gulp-msbuild');
+var conventions = require('./Conventions.js');
 
 gulp.task('default', ['minify-css']);
 
 gulp.task('minify-css', function () {
-    return gulp.src(env.solutionPath + '/Content/*.css')
+    return gulp.src(conventions.webPath() + 'Content/*.css')
     .pipe(minifyCSS({keepSpecialComments :0}))
     .pipe(concat("css-combined.min.css"))
     .pipe(rev())
-    .pipe(gulp.dest(env.solutionPath + 'Out/'));
+    .pipe(gulp.dest(conventions.buildPath()));
+});
+
+gulp.task("build", function () {
+    console.log(conventions.webProjectFile());
+    gulp.src(conventions.webProjectFile())
+        .pipe(msbuild({
+//            msbuildPath: "\"c:/Program Files (x86)/MSBuild/12.0/Bin/MSBuild.exe\"",
+//            msbuildPath: "\"C:/Windows/Microsoft.NET/Framework64/v4.0.30319/MSBuild.exe\"",
+            stdout: true,
+            stderr: true,
+            targets: ['Clean', 'Build', '_CopyWebApplication'],
+            toolsVersion: 4.0,
+            properties: {
+                Configuration: 'Release', 
+                VisualStudioVersion: '10.0', 
+                WebProjectOutputDir: conventions.buildPath() + "Web/",
+                OutputPath: conventions.buildPath() + "Web/",
+                OutDir: conventions.buildPath() + "Web/"
+            }
+        }));
 });
