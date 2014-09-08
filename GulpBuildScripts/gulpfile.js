b0c32cfcafae4309515c817bebec83d79be9231d
T Chandirasekar <chandru9279@gmail.com>
2014-09-08 19:19:34 +0530

    Changes to build options so that OutDir != OutputPath

--- GulpBuildScripts/gulpfile.js
+++ GulpBuildScripts/gulpfile.js
@@ -3,6 +3,7 @@ var minifyCSS = require('gulp-minify-css');
 var concat = require('gulp-concat');
 var rev = require('gulp-rev');
 var msbuild = require('gulp-msbuild');
+var xtend = require('xtend');
 var conventions = require('./Conventions.js');
 
 gulp.task('default', ['minify-css']);
@@ -15,22 +16,35 @@ gulp.task('minify-css', function () {
     .pipe(gulp.dest(conventions.buildPath()));
 });
 
-gulp.task("build", function () {
-    console.log(conventions.webProjectFile());
+gulp.task("build", function() {
+    var buildOptions = {
+        stdout: true,
+        stderr: true,
+        targets: ['Clean', 'Build'],
+        toolsVersion: 4.0,
+        properties: {
+            Configuration: 'Release',
+            OutDir: conventions.buildPath() + "Web"
+        }
+    };
+
+    console.log("Build and Clean: %j", buildOptions);
+
     gulp.src(conventions.webProjectFile())
-        .pipe(msbuild({
-//            msbuildPath: "\"c:/Program Files (x86)/MSBuild/12.0/Bin/MSBuild.exe\"",
-//            msbuildPath: "\"C:/Windows/Microsoft.NET/Framework64/v4.0.30319/MSBuild.exe\"",
-            stdout: true,
-            stderr: true,
-            targets: ['Clean', 'Build', '_CopyWebApplication'],
-            toolsVersion: 4.0,
-            properties: {
-                Configuration: 'Release', 
-                VisualStudioVersion: '10.0', 
-                WebProjectOutputDir: conventions.buildPath() + "Web/",
-                OutputPath: conventions.buildPath() + "Web/",
-                OutDir: conventions.buildPath() + "Web/"
-            }
-        }));
+        .pipe(msbuild(buildOptions));
 });
+
+/* TODO:
+
+Build Tasks 
+
+	* Clean
+	* Zip
+
+Deployment Tasks 
+
+	* To copy to shared folder of target webserver 
+	* RestartAppPool 
+	* Environment specific variables - deploy time.
+
+*/
--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 Gulp/Node based buildscripts for dotREZ
 
 This project requires the node.js extensions at http://nodejstools.codeplex.com/ to be installed into your VisualStudio.
+
+Download & installl node.js from http://nodejs.org/.
+
+Open the repository-directory/GulpBuildScripts from console and do an "npm install" to download npm packages BuildScripts depends upon.
+
+Use the helper files "gulp" (if you are using git bash) and "exec gulp" (if you are using command prompt) to execute gulp tasks.
+
+Go to dev.json, and change your dotRez repo path, if you have your dotRez repo in different location.
